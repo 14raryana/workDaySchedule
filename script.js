845d2e9f1670577a2eb90a73c244975a73ee30bc
Raman Aryana <14raryana@gmail.com>
2020-09-29 23:10:50 -0400

    fully functional workDaySchedule web app

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
-var currentDay = $("#currentDay");
-var currentDateTime = moment();
+// var currentDay = $("#currentDay");
+updateTime();
+
+setInterval(updateTime,1000);
+
+function updateTime(){
+    $("#currentDay").html(moment().format("dddd, MMMM Do") + "<br><br>" + moment().format("LTS"));
+}
+
+
+// function setTime(){
+
+//     currentDay.text(currentDate);
+//     currentDay.append("<br>");
+//     currentDay.text(currentTime);
+//     currentDay
+    
+//     currentDay.text()
+
+// }
+
+// var currentDateTime = moment();
 var currentTime = moment();
 var startTime = 9;
 var endTime = 18;
@@ -9,18 +29,19 @@ var endTime = 18;
 // console.log(setTime.format("HH:mm"));
 
 
-var randomTime = moment();
-randomTime = randomTime.format("HH:mm");
+// var randomTime = moment();
+// randomTime = randomTime.format("HH:mm");
+
+// currentDay.text(currentDateTime.format("[Today is ] dddd, MMMM Do <br><br> LTS"));
 
 
-currentDay.append(currentDateTime.format("[Today is ] dddd, MMMM Do <br><br> LT"));
 
-var test = moment("2010-10-20").isSame("2009-12-31");
-console.log(test);
-var test2 = moment("2010-10-20").isSame("2010-10-20");
-console.log(test2);
+// var test = moment("2010-10-20").isSame("2009-12-31");
+// console.log(test);
+// var test2 = moment("2010-10-20").isSame("2010-10-20");
+// console.log(test2);
 
-var hello = moment(currentDateTime.format("YYYY-MM-DD HH:mm")).isSame("21 PM","hour");
+// var hello = moment(currentDateTime.format("YYYY-MM-DD HH:mm")).isSame("21","hour");
 
 // console.log(currentDateTime.format("LT"));
 
@@ -53,11 +74,13 @@ for(var i=startTime; i<endTime; i++) {
     // creating text area
     var textArea = $("<textarea>");
     textArea.addClass("col-md-10 description");
+    // textArea.attr("id","hour-"+i);
+    textArea.attr("data-time-text", "hour-"+i);
 
     // creating button
     var btn = $("<button>");
     btn.addClass("btn saveBtn col-md-1");
-    btn.attr("data-time","hour-"+i);
+    btn.attr("data-time-btn","hour-"+i);
 
     // creating save icon
     var icon = $("<i>");
@@ -92,5 +115,48 @@ for(var i=startTime; i<endTime; i++) {
 }
 
 $(".btn").on("click", function(){
-    alert($(this).attr("data-time"));
+    var textArea = $(this).parent().find("textarea");
+    var key = textArea.attr("data-time-text");
+    var value = textArea.val();
+    localStorage.setItem(key, value);
+
+    console.log(value);
+    // var clickedBtnId = $(this).attr("data-time-btn");
+    // // var textAreaId = $()
+    // console.log(clickedBtnId);
+    // getValue(clickedBtnId)
+
+    // for(i=startTime; i < endTime; i++){
+    // }
+
+
+    // alert(clickedBtn);
+    // var shit = $("textarea");
+    // console.log(shit);
+
+    // console.log($(".description #hour-13"));
+
+    // var keyNote = $(this).attr("data-time-btn");
+    // var note = $(".description").attr("data-time-text");
+    // localStorage.setItem(keyNote, note);
 })
+var allDivs = $(".time-block");
+for(var i = 0; i < allDivs.length; i++){
+    var id = allDivs[i].id;
+    var savedValue = localStorage.getItem(id);
+    var textArea = allDivs[i].querySelector("textarea");
+    textArea.innerHTML = savedValue;
+    console.log(textArea);
+    // allDivs[i].find("textarea").text(savedValue);
+}
+
+
+// function getValue(textAreaId){
+//     var selector = "textarea[data-time-text = " + textAreaId + "]";
+//     var selectedTextArea = $(selector);
+//     var textAreaValue = selectedTextArea.val();
+//     console.log(textAreaValue);
+//     localStorage.setItem(textAreaId, textAreaValue);
+//     // console.log($("textarea[data-time-text = " + textAreaId + "]"));
+//     // console.log($("#" + textAreaId).val());
+// }
